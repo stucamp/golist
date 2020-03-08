a3cf97c14439433de3e8b37b3bc7a479a5b5028f
Stu Campbell <stucampbell.git@gmail.com>
2020-03-08 14:56:49 -0700

    import name change

--- linkedlist.go
+++ linkedlist.go
@@ -1,4 +1,4 @@
-package linkedlist
+package golist
 
 import (
 	"fmt"
---
+++ try/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	linkedlist "github.com/stucamp/golist"
+)
+
+func main() {
+	lst := linkedlist.List{}
+
+	lst.Insert(5)
+
+	lst.Display()
+}
